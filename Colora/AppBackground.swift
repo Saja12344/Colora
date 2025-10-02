4923452e60ffd0835b1e1b2d019e2424a209d08d
NorahShaya <norahalshaya4@gmail.com>
2025-10-02 11:29:27 +0300

    Adding home page

---
+++ Colora/AppBackground.swift
@@ -0,0 +1,18 @@
+//
+//  AppBackground.swift
+//  Colora
+//
+//  Created by Norah Aldawsari on 10/04/1447 AH.
+//
+
+import SwiftUI
+
+struct AppBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppBackground()
+}
